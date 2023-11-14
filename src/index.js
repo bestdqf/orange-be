b6308e8917f64efd3bbfbe8919849e66d6b793ac
duanqifeng <bestdqf@163.com>
2023-11-14 23:39:58 +0800

    t

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ router.get('/',async (ctx)=>{
 })
 
 
+router.get('/p1',async (ctx)=>{
+  ctx.body="p1";
+
+})
+
+
 app.use(router.routes());   /*启动路由*/
 app.use(router.allowedMethods());
 app.listen("8001", () => console.log("服务端启动"));
