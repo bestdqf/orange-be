6a2415c43b4acadad0187b809fd78b579eb026b5
duanqifeng <bestdqf@163.com>
2023-11-14 23:52:44 +0800

    t

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const router = new Router();
 router.get('/', (ctx, next) => {
   // ctx.router available
   ctx.body="root"
-  next()
+  return next()
 })
 
 
 router.get('/users', (ctx, next) => {
   ctx.body="users"
-  next()
+  return next()
 })
 router.get('/users/:id', (ctx, next) => {
   console.log(ctx)
