30a8846a92445f3e7f40b93bb700306652fbf0ae
duanqifeng <bestdqf@163.com>
2023-11-14 23:39:07 +0800

    t

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
-const Koa=require('Koa')
-// 导入koa-router
-const Router=require('koa-router')
-const app=new Koa();
-// 创建路由对象
-const router=new Router();
-// const fs=require('fs')
-const bodyParser = require('koa-bodyparser');
-app.use(bodyParser());
-// 使用路由中间件
-app.use(router.routes());
-router.get('/',(ctx)=>{
+var Koa=require('koa');
 
-  console.log(ctx)
+var router = require('koa-router')();  /*引入是实例化路由** 推荐*/
 
-    ctx.body={a:1}
+//实例化
+var app=new Koa();
+
+router.get('/',async (ctx)=>{
+    ctx.body="首页";
+
 })
-// router.get('/login',(ctx)=>{
-//     ctx.type='html'
-//     ctx.body=fs.createReadStream('./login.html')
-// })
-// router.post('/userLogin',(ctx)=>{
-//     ctx.body='用户名是:'+ctx.request.body.userName
-// })
-// router.get('/users/:id',(ctx)=>{
-//     ctx.body='这是用户的编号:'+ctx.params.id
-// })
+
+
+app.use(router.routes());   /*启动路由*/
+app.use(router.allowedMethods());
 app.listen("8001", () => console.log("服务端启动"));
 
