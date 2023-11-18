4a32ede9c5823845a60e2c8cadb234964fadc47c
duanqifeng <duanqifeng@jd.com>
2023-11-18 22:04:25 +0800

    t

--- src/index.js
+++ src/index.js
@@ -1,46 +1,30 @@
-const Koa = require('koa');
-const Router = require('@koa/router');
-// const {get} = require('./request')
-const axios = require('axios')
+const Koa = require("koa");
+const Router = require("@koa/router");
+const axios = require("axios");
 
 const app = new Koa();
 const router = new Router();
 
+router.get("/", (ctx, next) => {
+  ctx.body = "root";
+  return next();
+});
 
-  // axios.get( `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx81f25331c0868425&secret=78ff6f8ee843494161a16dd47d058c14&code=${1}&grant_type=authorization_code`,).then(data=>{
-  //   console.log(data)
-  // })
+router.get("/login", async (ctx, next) => {
+  const { code } = ctx.request.query;
 
-router.get('/', (ctx, next) => {
-  ctx.body="root"
-  return next()
-})
-
-
-
-
-router.get('/login', async  (ctx, next) => {
-  // console.log(ctx.request.query)
-
-  const {code} = ctx.request.query
-
-   const a = await axios.get( `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx81f25331c0868425&secret=78ff6f8ee843494161a16dd47d058c14&code=${code}&grant_type=authorization_code`,)
-
-
-   console.log(a)
-
-  ctx.body={a:2}
-  return next()
-})
+  const a = await axios.get(
+    `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx81f25331c0868425&secret=78ff6f8ee843494161a16dd47d058c14&code=${code}&grant_type=authorization_code`
+  );
+  ctx.body = a;
+  return next();
+});
 // router.get('/users/:id', (ctx, next) => {
 //   console.log(ctx)
 //   ctx.body="users,id"
 //   next()
 // })
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen("8001", () => console.log("服务端启动"));
-
